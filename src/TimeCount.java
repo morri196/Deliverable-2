42d527f3076c4b041a035cca3ab72211f2bc146a
Kari Mcfarlin <40720949+morri196@users.noreply.github.com>
2018-07-09 01:27:47 -0400

    Update TimeCount.java

--- src/TimeCount.java
+++ src/TimeCount.java
@@ -1,39 +1,47 @@
-import java.util.Scanner;
 //NumberCompatibility *Spell Check*
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
 
-public class TimeCount {
-	public static void main(String [] args) {
-		Scanner scnr = new Scanner(System.in);//allows for user input
-		
-		int monthNum;
-		int dayNum;
-		int yearNum;
-		int monthNum2;
-		int dayNum2;
-		int yearNum2; // i used int because i want the user input in numbers only
-		
-		monthNum = scnr.nextInt();
-		monthNum2 = scnr.nextInt();
-		dayNum = scnr.nextInt();
-		dayNum2 = scnr.nextInt();
-		yearNum = scnr.nextInt();
-		yearNum2 = scnr.nextInt();
-		
-		System.out.print("Enter a date. Ex. (09/23/1984) using numerical expressions only:  ");
-		System.out.println(monthNum + "/" + dayNum + "/" + yearNum); // I want this line to appear on the same line as "Enter a Date
-		System.out.print("Enter a second date greater than the first. Ex. (09/23/2004):  "); //I want this line and the next to appear on one line together
-		System.out.println(monthNum2 + "/" + dayNum2 + "/" + yearNum2);
-		
-		System.out.print("Now let see how much time is inbetween the two dates you entered");
-		
-		/*next Statement with need to express and equation that wold give a number that 
-		 * expresses the amount of time inbetewen the dates from the user input*/
-		 
-		Number of months 
-		
-		int numDate = 'monthNum' 'dayNum' 'yearNum';
-		
-		
+public class TimeCount{
+   public static void main(String[] args) {
+	   Scanner scnr = new Scanner(System.in);
+	  
+	   
+	   
+	   int year1;
+	   int month1;
+	   int day1;
+	   int year2;
+	   int month2;
+	   int day2;
+	   int entry1;
+	   int entry2;
+	   int entryOne;
+	   int entryTwo;
+	   String entryOnetxt;
+	   String entryTwotxt;
+	     
+	   
+	  System.out.println("Enter a date. ex. MMDDYYYY: ");//get input from user
+	   entryOne = scnr.nextInt();
+	   System.out.println("Enter a second date: ");
+	   entryTwo = scnr.nextInt();
+	   
+	  
+	   entryOnetxt = Integer.toString(entryOne);//covert int to string because entry is an integer
+	   entryTwotxt = Integer.toString(entryTwo);
+	   
+	   LocalDate firstDate1 = LocalDate.of(Integer.parseInt(entryOnetxt.substring(3)),Integer.parseInt(entryOnetxt.substring(1,3)),Integer.parseInt(entryOnetxt.substring(0,2)));
+	   LocalDate secondDate2 = LocalDate.of(Integer.parseInt(entryTwotxt.substring(3)),Integer.parseInt(entryTwotxt.substring(1,3)),Integer.parseInt(entryTwotxt.substring(0,2)));
+	   //converts the entry into date format
+	  
+			
+		Period diff = Period.between(firstDate1, secondDate2);	//allows for time difference to be calculated
+			
+	  
+	   System.out.printf("DIfference is %d years, %d months and %d days", //this prints what the difference is!!!!!!YAY!!!!!
+				diff.getYears(), diff.getMonths(), diff.getDays());
 		
 	}
 
